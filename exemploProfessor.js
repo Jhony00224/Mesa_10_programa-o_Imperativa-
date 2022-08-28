cf410539b0a36faeea3401f13fda54e375484d04
Jhony00224 <biubio70@gmail.com>
2022-08-28 17:26:48 -0400

    commit

---
+++ exemploProfessor.js
@@ -0,0 +1,18 @@
+let meuarray = ['Star Wars', 'huuu', 23, 'meu nome', 'jhony'];
+
+
+console.log(meuarray.length);
+
+//com esse codigo a gente munda o separador que são as vigulas//
+console.log(meuarray.join(' () '));
+
+//meuarray.push('mundo destruido');
+
+//console.log(meuarray);
+
+//console.log(meuarray.length);
+
+//console.log(meuarray[3]+' '+meuarray[4]);
+//console.log(meuarray[0]);
+//console.log(meuarray[3]);
+//console.log(meuarray[0]+' '+meuarray[2]);
